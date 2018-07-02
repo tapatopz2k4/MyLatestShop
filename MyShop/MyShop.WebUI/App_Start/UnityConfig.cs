b24e75638e86e952c00d18c72ca45c395a364f26
tapatopz2k4 <39274060+tapatopz2k4@users.noreply.github.com>
2018-07-02 14:47:05 -0500

    Added SQL Repository

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -1,5 +1,6 @@
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.DataAccess.SQL;
 using MyShop.DataAccessInMemory;
 using System;
 
@@ -45,8 +46,10 @@ namespace MyShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            //container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
+            //container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
