900eed57bd718cc81a25f3555d4843767a94e74e
tapatopz2k4 <39274060+tapatopz2k4@users.noreply.github.com>
2018-07-03 09:54:43 -0500

    Added Basket models

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BAseEntity
+    {
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
